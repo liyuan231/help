314de66a55e96f750ef790284fed3250941cdbff
Liyuan <1987151116@qq.com>
2020-02-08 17:16:16 +0800

    init repo

---
+++ src/main/java/press/liyuan/help_a/HelpAApplication.java
@@ -0,0 +1,13 @@
+package press.liyuan.help_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/press/liyuan/help_a/HelpAApplicationTests.java
@@ -0,0 +1,13 @@
+package press.liyuan.help_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
